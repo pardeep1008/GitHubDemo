ead588799b1de577f26621c0081ac70f7d518274
pardeep1008 <pardeep1008@gmail.com>
2017-03-10 11:48:13 +0530

    Added new third controller file for ABC module

---
+++ GitDemoApp/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  GitDemoApp
+//
+//  Created by Pardeep Kumar on 10/03/2017.
+//  Copyright © 2017 Chetu. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    print("")
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
